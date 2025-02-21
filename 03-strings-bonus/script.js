b81b37149487d74c980676993a0ebc793a0ee08b
lorenzosalti <lorenzosalti@gmail.com>
2025-02-21 16:37:32 +0100

    feat: bonus 2. and 3. complete

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -11,12 +11,12 @@ console.log(cityIndex); // Output: 4
 
 // 2. Sottostringhe 
 // Stampa la sottostringa "York" dalla variabile city
-const citySubstring = '';
+const citySubstring = city.substring(4);
 console.log(citySubstring); // Output: "York"
 
 // 3. Sostituzione
 // Sostituisci 'York' con 'Delhi'
-const cityReplaced = '';
+const cityReplaced = city.replace('York', 'Delhi');
 console.log(cityReplaced); // Output:  "New Delhi"
 
 // 4. Template literal
